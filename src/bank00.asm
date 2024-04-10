4e5cb7ebdece665e9d38d19d7a5c4725f2dd3765
xenophile127 <dev@null.com>
2024-04-10 13:19:03 -0700

    Move the default line effect into a separate include.

--- src/bank00.asm
+++ src/bank00.asm
@@ -455,20 +455,7 @@ loadLCDCEffectBuffer:
 
 ds 11 ; Free space
 
-; Load the default LCDC effect buffer, which handles the status bar not having sprites
-; on top and the forces the color palette of the status bar.
-setDefaultLCDCEffect:
-    ld   A, $7e                                        ;; 00:0313 $3e $7e
-    ldh  [rLYC], A                                     ;; 00:0315 $e0 $45
-    ld   A, $00                                        ;; 00:0317 $3e $00
-    ld   [wLCDCEffectIndex], A                         ;; 00:0319 $ea $e2 $d3
-    ld   DE, wLCDCEffectBuffer                         ;; 00:031c $11 $a0 $d3
-    ld   HL, .lcdcDefaultEffect ;@=ptr .lcdcDefaultEffect ;; 00:031f $21 $28 $03
-    ld   B, $05                                        ;; 00:0322 $06 $05
-    call copyHLtoDE                                    ;; 00:0324 $cd $49 $2b
-    ret                                                ;; 00:0327 $c9
-.lcdcDefaultEffect:
-    db   $7e, $fc, $01, $e4, $ff                       ;; 00:0328 .....
+INCLUDE "code/line-effects/default.asm"
 
 LCDCInterrupt:
     ld   A, [wLCDCEffectIndex]                         ;; 00:032d $fa $e2 $d3
