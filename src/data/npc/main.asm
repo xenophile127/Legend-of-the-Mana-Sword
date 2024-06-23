ad2f0346fc5988f59019d79fcfcf447e1d1ca909
xenophile127 <dev@null.com>
2024-06-23 01:07:51 -0700

    Prevent Grells (floating jellyfish enemies) from flipping horizontally.
    The only difference between the left and right sides is a highlight which is more obvious in color.

--- src/data/npc/main.asm
+++ src/data/npc/main.asm
@@ -41,7 +41,7 @@ ENDC
     NPC_DATA $91, $11, $50, $06, gfxEnemyRabite, tileorderNpc, metaspritesRabiteFlowerFlameRed, $00, $01, $02, $00, $00, $00, $0f, $10, $10, $10, script_003d, $0000 ;; 03:60f2 NPC_RABBITE
     NPC_DATA $91, $12, $40, $08, gfxEnemyGoblin, tileorderNpc, metaspritesTabel40Normal, $00, $01, $00, $00, $00, $00, $02, $02, $03, $03, script_003d, script_0438 ;; 03:610a NPC_GOBLIN
     NPC_DATA $91, $13, $50, $04, gfxEnemyMyconid, tileorderNpc, metaspritesMyconid, $00, $01, $01, $00, $00, $00, $02, $02, $02, $02, script_003d, $0000 ;; 03:6122 NPC_MUSHROOM
-    NPC_DATA $93, $14, $40, $04, gfxEnemyGrell, tileorderNpc, metaspritesTable40FrontFacing, $00, $01, $01, $00, $00, $00, $12, $12, $0b, $0b, script_003d, $0000 ;; 03:613a NPC_JELLYFISH
+    NPC_DATA $93, $14, $40, $04, gfxEnemyGrell, tileorderNpc, metaspritesRoper, $00, $01, $01, $00, $00, $00, $12, $12, $0b, $0b, script_003d, $0000 ;; 03:613a NPC_JELLYFISH
     NPC_DATA $91, $15, $50, $04, gfxEnemyMudman, tileorderNpc, metaspritesTable50FrontFacing, $01, $01, $00, $00, $00, $00, $16, $16, $16, $16, script_003d, script_044b ;; 03:6152 NPC_SWAMPMAN
     NPC_DATA $91, $16, $60, $08, gfxEnemyLizardman, tileorderNpc, metaspritesTabel60Normal, $00, $01, $01, $00, $00, $00, $02, $02, $03, $03, script_003d, script_043a ;; 03:616a NPC_LIZARDMAN
     NPC_DATA $91, $17, $50, $04, gfxEnemyDeathFlower, tileorderNpc, metaspritesRabiteFlowerFlameRed, $00, $01, $01, $00, $01, $00, $0d, $00, $0d, $0d, script_003d, script_043c ;; 03:6182 NPC_FLOWER
