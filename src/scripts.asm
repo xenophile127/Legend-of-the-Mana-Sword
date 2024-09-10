63ef89096f14e030deb3ba4469b94eccf252a75d
xenophile127 <dev@null.com>
2024-09-09 19:03:53 -0700

    In the cutscene after Megapede walk Watts as far off the screen as possible instead of hiding him while he's still fully onscreen..

--- src/scripts.asm
+++ src/scripts.asm
@@ -8617,6 +8617,7 @@ script_0435:
     sFOLLOWER_DIRECTION_RIGHT                          ;; 0e:54ae $96
     sFOLLOWER_STEP_FORWARD                             ;; 0e:54af $90
     sFOLLOWER_STEP_FORWARD                             ;; 0e:54b0 $90
+    ;sFOLLOWER_STEP_FORWARD
     sUNK_9B                                            ;; 0e:54b1 $9b
     sFOLLOWER_DELETE                                   ;; 0e:54b2 $98
     sCLEAR_FLAG wScriptFlags0A.3                       ;; 0e:54b3 $db $53
